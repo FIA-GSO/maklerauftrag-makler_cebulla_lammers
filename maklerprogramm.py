c6cd03742cb92553dcea582eefacc7caa6d96468
Jonas Lammers <jonaselias.l@gso.schule.koeln>
2024-02-07 13:23:44 +0100

    Database appending is now working

--- maklerprogramm.py
+++ maklerprogramm.py
@@ -8,6 +8,7 @@
 
 import sqlite3
 gesamtflaeche = 0
+raumid = 0
 
 # Get highest number from the database and store it as "ObjektID" 
 
@@ -24,12 +25,15 @@ print("ObjektID ist: " + str(ObjektID))
 
 # get the number of rooms to calculate
 
-raumanzahl = input("Anzahl der Räume:")
+raumanzahl = int(input("Anzahl der Räume:"))
+verbleibenderaeume = raumanzahl
+
 
 # calculate every room for the number of rooms to calculate
 
-for i in raumanzahl:
-    raumid = i
+while verbleibenderaeume > 0:
+    raumid = raumid + 1
+    print("\nRaum " + str(raumid))
     laengstewand = input("Angabe der längsten Wand (in cm):")
     angrenzendewand = input("90° angrenzende Wand (in cm):")
     raumflaeche = int(laengstewand) * int(angrenzendewand)
@@ -50,10 +54,21 @@ for i in raumanzahl:
             zusatzflaeche = zusatzflaeche / 10000
             raumflaeche = raumflaeche - zusatzflaeche
         weitere_flaeche = input("Soll eine weitere Fläche hinzugefügt werden? (y/n)")
-    print(raumflaeche + "m2")
+    print(str(raumflaeche) + " m2 Raumfläche")
     gesamtflaeche = gesamtflaeche + raumflaeche
 
     conn = sqlite3.connect("Maklerdatenbank.db")
     cursor = conn.cursor()
+    cursor.execute(f"INSERT INTO raeume (ObjektID, RaumID, Raumgroesse) VALUES ({ObjektID}, {raumid}, {raumflaeche})")
+    conn.commit()
     conn.close()
 
+    verbleibenderaeume = verbleibenderaeume - 1
+
+
+print("Gesamtfläche: " + str(gesamtflaeche))
+conn = sqlite3.connect("Maklerdatenbank.db")
+cursor = conn.cursor()
+cursor.execute(f"INSERT INTO Objekte (ObjektID, ObjektName, Gesamtflaeche, Raumanzahl) VALUES ({ObjektID}, '{Objektname}', {gesamtflaeche}, {raumanzahl})")
+conn.commit()
+conn.close()
